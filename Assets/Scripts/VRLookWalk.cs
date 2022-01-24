005e1e8af06456727383d316b563c13cda979685
Dylan Cody Moore <dylan.moore@section6.tech>
2022-01-24 14:21:52 +0900

    Task #2 Finished
    Task #2 Completed
    
    2. Add a sound that will last through the whole game while is on "Start" mode. Add a second sound when getting nearby an object.

---
+++ Assets/Scripts/VRLookWalk.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class VRLookWalk : MonoBehaviour
+{
+    // VR Main Camera
+    public Transform vrCamera;
+    // Agol spored koj se realizira dvizenjeto na igracot
+    public float toggleAngle = 30.0f;
+    // Brzina so koja ke se dvizi igracot
+    public float speed = 3.0f;
+    // Promenliva koja ke regulira dali moze da se dvizi igracot ili ne 
+    public bool moveForward;
+    // CharacterController scripta
+    public CharacterController cc;
+    void Start()
+    {
+        //inicijalizacija na CharacterController
+        cc = GetComponentInParent<CharacterController>();
+    }
+    void Update()
+    {
+        // dokolku gleda pomalku od 90 degree toa znaci gleda nadolu i moze da se dvizi
+        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
+        {
+            //Moze da se dvizi
+            moveForward = true;
+        }
+        else
+        {
+            //Ne moze da se dvizi
+            moveForward = false;
+        }
+        //Dokolku moze da se dvizi igracot
+        if (moveForward)
+        {
+            //Se naoga nasokata na dvizenje 
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            //so ova se izveduva dvizenjeto/pomestuvanjeto
+            cc.SimpleMove(forward * speed);
+        }
+    }
+}
+
+
