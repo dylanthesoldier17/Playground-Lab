5deccbb3bdd111849d27714391b71b6a0d5ca321
Dylan Cody Moore <dylan.moore@section6.tech>
2022-01-24 23:36:38 +0900

    Created Leaderboard
    Created the base functionality for the leaderboard

---
+++ Assets/Scripts/LeaderBoard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+[Serializable]
+public class LeaderBoard
+{
+    //Put size of the leaderboard array to 6, so the last spot can be used to add and sort scores.
+    public int[] scores;
+    private int numberOfScoresToHold = 6;
+    
+    public LeaderBoard()
+    {
+        InstantiateLeaderBoard();
+    }
+    
+    private void InstantiateLeaderBoard()
+    {
+        scores = new int[numberOfScoresToHold];
+    }
+    
+    public int GetScore(int index)
+    {
+        return scores[index];
+    }
+
+    public void SetScore(int index, int score)
+    {
+        scores[index] = score;
+    }
+
+    public void AddScore(int score)
+    {
+        scores[scores.Length - 1] = score;
+        Array.Sort(scores);
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public string saveFileName;
+    private string saveFilePath;
+    private LeaderBoard leaderBoard;
+    public Text[] scoreTextObjects;
+
+    private void Awake()
+    {
+        saveFilePath = Application.persistentDataPath + "/" + saveFileName;
+        
+        loadFromJson(saveFilePath);
+        if (leaderBoard == null)
+            leaderBoard = new LeaderBoard();
+
+        updateScoreTextObjects();
+    }
+
+    public void addToLeaderBoard(int score)
+    {
+        leaderBoard.AddScore(score);
+        updateScoreTextObjects();
+    }
+
+    private void updateScoreTextObjects()
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            scoreTextObjects[i-1].text = $"#{i} - {leaderBoard.GetScore(i).ToString()} coins";
+        }
+    }
+    
+    public void saveToJson(string path)
+    {
+        string json = JsonUtility.ToJson(leaderBoard);
+        if (!Directory.Exists(Application.persistentDataPath))
+            Directory.CreateDirectory(Application.persistentDataPath);
+        File.WriteAllText(path, json);
+    }
+
+    public void loadFromJson(string path)
+    {
+        try
+        {
+            leaderBoard = JsonUtility.FromJson<LeaderBoard>(path);    
+        }catch(Exception exception)
+        {}
+    }
+
+    private void OnDestroy()
+    {
+        saveToJson(saveFilePath);
+    }
+}
+
