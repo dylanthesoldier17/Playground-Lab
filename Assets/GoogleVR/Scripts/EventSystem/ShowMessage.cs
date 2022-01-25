a81742880a674a4c726b7507213f787588b8c171
Dylan Cody Moore <dylan.moore@section6.tech>
2022-01-25 22:41:24 +0900

    Fixed Raycast and Dialog in Build
    Had to add a Physics Raycast object to make the dialog show in the build. Works regardless in playmode.

---
+++ Assets/GoogleVR/Scripts/EventSystem/ShowMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ShowMessage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string message;
+    public TMP_Text textObject;
+    public GameObject messageGameObject;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        textObject.text = message;
+        messageGameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        messageGameObject.SetActive(false);
+    }
+}
